168b65370a8ce21eda40f798bf43b99001e2efdb
han-keong <hk997@live.com>
2020-03-27 20:18:16 +0800

    added junit test templates for all methods

--- src/com/routingengine/test/methods/TestActivateAgentMethod.java
+++ src/com/routingengine/test/methods/TestActivateAgentMethod.java
@@ -12,7 +12,7 @@ import com.routingengine.client.ClientConnectionHandler;
 import com.routingengine.json.JsonResponse;
 
 
-public class TestWaitForAgentMethod
+public class TestActivateAgentMethod
 {   
     private static Client client;
     private static final String hostname = "localhost";
--- src/com/routingengine/test/methods/TestUpdateAgentSkillsMethod.java
+++ src/com/routingengine/test/methods/TestUpdateAgentSkillsMethod.java
@@ -1,6 +1,97 @@
 package com.routingengine.test.methods;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static com.routingengine.json.JsonUtils.*;
+import java.io.IOException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import com.routingengine.Logger;
+import com.routingengine.client.Client;
+import com.routingengine.client.ClientConnectionHandler;
+import com.routingengine.json.JsonResponse;
+
 
 public class TestUpdateAgentSkillsMethod
-{
+{   
+    private static Client client;
+    private static final String hostname = "localhost";
+    private static final int port = 50000;
+    
+    @BeforeAll
+    static void setUpBeforeClass()
+        throws Exception
+    {
+        client = new Client(hostname, port);
+    }
+    
+    @AfterAll
+    static void tearDownAfterClass()
+        throws Exception
+    {
+        client.close();
+    }
+    
+    @Test
+    void test()
+        throws IOException
+    {
+        Logger.log("hello from test");
+        
+        client.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            protected void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                JsonResponse response = ping();
+                Logger.log(response.toString());
+                
+                assertEquals("pong", castToString(response.getPayload()));
+            }  
+        });
+        
+        client.run();
+    }
+    
+    @Test
+    void test2()
+        throws IOException
+    {
+        Logger.log("hello from test2");
+        
+        client.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            protected void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                JsonResponse response = ping();
+                Logger.log(response.toString());
+                
+                assertEquals("pong", castToString(response.getPayload()));
+            }  
+        });
+        
+        client.run();
+    }
+    
+    @Test
+    void test3()
+        throws IOException
+    {
+        Logger.log("hello from test3");
+        
+        client.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            protected void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                JsonResponse response = ping();
+                Logger.log(response.toString());
+                
+                assertEquals("pong", castToString(response.getPayload()));
+            }  
+        });
+        
+        client.run();
+    }
 }
