a58b122e2f6d4fdc80ec281c987f2dad19d31d50
han-keong <hk997@live.com>
2020-03-29 14:57:00 +0800

    added comments about each package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.routingengine
 {
     // Features
-    exports com.routingengine.server;
-    exports com.routingengine.client;
-    exports com.routingengine.json;   // JSON tools and protocol that will be useful for development
+    exports com.routingengine;          // Core logic of the routing engine
+    exports com.routingengine.methods;  // Methods that are used by the routing engine
+    exports com.routingengine.server;   // Server program for the routing engine
+    exports com.routingengine.client;   // Client interface to communicate with the routing engine server
+    exports com.routingengine.json;     // JSON tools and protocol that will be useful for development
     
     // Dependencies
     requires transitive com.google.gson;
