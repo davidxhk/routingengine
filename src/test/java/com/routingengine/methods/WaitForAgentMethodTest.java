3de2f5a854119d12263d33f0f0af07cd5a70be5c
han-keong <hk997@live.com>
2020-03-30 00:58:55 +0800

    completed WaitForAgentMethodTest

--- src/test/java/com/routingengine/methods/WaitForAgentMethodTest.java
+++ src/test/java/com/routingengine/methods/WaitForAgentMethodTest.java
@@ -1,152 +1,366 @@
 package com.routingengine.methods;
 
-import static com.routingengine.Logger.log;
 import static org.junit.jupiter.api.Assertions.*;
 import static com.routingengine.json.JsonUtils.*;
 import java.io.IOException;
-import java.util.Map;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.google.gson.JsonObject;
-import com.routingengine.client.Client;
+import com.routingengine.MethodTestBase;
+import com.routingengine.SupportRequest;
 import com.routingengine.client.ClientConnectionHandler;
+import com.routingengine.json.JsonRequest;
 import com.routingengine.json.JsonResponse;
 
 
-public class WaitForAgentMethodTest
+public class WaitForAgentMethodTest extends MethodTestBase
 {   
-    private static Client client, client2;
-    private static final String hostname = "localhost";
-    private static final int port = 50000;
-    private volatile String supportRequestUUIDString, agentUUIDString;
-    private Thread client2Thread;
-    
-    @BeforeAll
-    static void setUpBeforeClass()
-        throws Exception
-    {
-        client = new Client(hostname, port);
-        client2 = new Client(hostname, port);
-    }
-    
-    @AfterAll
-    static void tearDownAfterClass()
-        throws Exception
-    {
-        client.close();
-        client2.close();
-    }
-    
-    @BeforeEach
-    void setUpBeforeEach()
-        throws IOException
-    {
-        client2.setConnectionHandler(new ClientConnectionHandler()
-        {
-            @Override
-            public void runMainLoop()
-                throws IOException, InterruptedException
-            {
-                JsonResponse response = newSupportRequest("bob", "bob@abc.com", 1);
-                JsonObject payload = castToJsonObject(response.getPayload());
-                supportRequestUUIDString = getAsString(payload, "uuid");
-                
-                response = newAgent(Map.of(1, true));
-                payload = castToJsonObject(response.getPayload());
-                agentUUIDString = getAsString(payload, "uuid");
-                updateAgentAvailability(agentUUIDString, true);
-                takeSupportRequest(agentUUIDString);
-            }
-        });
-        
-        client2Thread = new Thread(client2);
-        client2Thread.run();
-    }
-    
-    @AfterEach
-    void tearDownAfterEach()
-        throws IOException
-    {
-        client2Thread.interrupt();
-        client2.setConnectionHandler(new ClientConnectionHandler()
-        {
-            @Override
-            public void runMainLoop()
-                throws IOException, InterruptedException
-            {
-                closeSupportRequest(supportRequestUUIDString);
-                removeAgent(agentUUIDString);
-            }
-        });
-        
-        client2Thread = new Thread(client2);
-        client2Thread.run();
-        
-    }
+    protected static final String method = "wait_for_agent";
     
     @Test
+    @DisplayName("Test 1.1 - Valid uuid")
     void test01()
-        throws IOException
+        throws IOException, InterruptedException, ExecutionException
     {
-        log("test01 supportRequestUUID: " + supportRequestUUIDString + ", agentUUID: " + agentUUIDString);
+        final String supportRequestUUIDString = generateNewSupportRequest();
         
-        client.setConnectionHandler(new ClientConnectionHandler() {
+        final String agentUUIDString = generateNewAgent();
+        
+        agentTakesSupportRequest(agentUUIDString);
+        
+        customer.setConnectionHandler(new ClientConnectionHandler() {
             @Override
             public void runMainLoop()
                 throws IOException, InterruptedException
             {
-                JsonResponse response = ping();
-                log(response.toString());
+                JsonResponse response = waitForAgent(supportRequestUUIDString);
                 
-                assertEquals("pong", castToString(response.getPayload()));
-            }  
+                assertEquals(method, response.getMethod());
+                
+                assertTrue(response.didSucceed());
+                
+                JsonObject payload = castToJsonObject(response.getPayload());
+                
+                assertNotNull(payload);
+                
+                SupportRequest supportRequest = SupportRequest.fromJson(payload);
+                
+                assertTrue(supportRequest.hasAssignedAgent());
+            }
         });
         
-        client.run();
+        customer.run();
+        
+        removeSupportRequest(supportRequestUUIDString);
+        
+        removeAgent(agentUUIDString);
     }
     
     @Test
+    @DisplayName("Test 2.1 - Missing uuid")
     void test02()
         throws IOException
     {
-        log("test02 supportRequestUUID: " + supportRequestUUIDString + ", agentUUID: " + agentUUIDString);
-        
-        client.setConnectionHandler(new ClientConnectionHandler() {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
             @Override
             public void runMainLoop()
                 throws IOException, InterruptedException
             {
-                JsonResponse response = ping();
-                log(response.toString());
+                new JsonRequest()
+                    .setMethod(method)
+                    .writeSafe(jsonWriter);
                 
-                assertEquals("pong", castToString(response.getPayload()));
-            }  
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("uuid missing", error);
+            }
         });
         
-        client.run();
+        customer.run();
     }
     
     @Test
+    @DisplayName("Test 2.2.1 - Invalid uuid case 1")
     void test03()
         throws IOException
     {
-        log("test03 supportRequestUUID: " + supportRequestUUIDString + ", agentUUID: " + agentUUIDString);
-        
-        client.setConnectionHandler(new ClientConnectionHandler() {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
             @Override
             public void runMainLoop()
                 throws IOException, InterruptedException
             {
-                JsonResponse response = ping();
-                log(response.toString());
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("uuid", new ArrayList<>())
+                    .writeSafe(jsonWriter);
                 
-                assertEquals("pong", castToString(response.getPayload()));
-            }  
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("uuid invalid", error);
+            }
         });
         
-        client.run();
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 2.2.2 - Invalid uuid case 2")
+    void test04()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("uuid", new HashMap<>())
+                    .writeSafe(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("uuid invalid", error);
+            }
+        });
+        
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 2.2.3 - Invalid uuid case 3")
+    void test05()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("uuid", "hahaha test test")
+                    .writeSafe(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("uuid invalid", error);
+            }
+        });
+        
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 3.1 - Missing input")
+    void test06()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .writeSafe(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("uuid missing", error);
+            }
+        });
+        
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 3.2 - Unexpected arguments")
+    void test07()
+        throws IOException, InterruptedException, ExecutionException
+    {
+        final String supportRequestUUIDString = generateNewSupportRequest();
+        
+        final String agentUUIDString = generateNewAgent();
+        
+        agentTakesSupportRequest(agentUUIDString);
+        
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("uuid", supportRequestUUIDString)
+                    .setArgument("something", "something?")
+                    .writeSafe(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertTrue(response.didSucceed());
+                
+                JsonObject payload = castToJsonObject(response.getPayload());
+                
+                assertNotNull(payload);
+                
+                SupportRequest supportRequest = SupportRequest.fromJson(payload);
+                
+                assertTrue(supportRequest.hasAssignedAgent());
+            }
+        });
+        
+        customer.run();
+        
+        removeSupportRequest(supportRequestUUIDString);
+        
+        removeAgent(agentUUIDString);
+    }
+    
+    @Test
+    @DisplayName("Test 3.3.1 - Malformed arguments case 1")
+    void test08()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + " \"testtest\"");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("malformed arguments", error);
+            }
+        });
+        
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 3.3.2 - Malformed arguments case 2")
+    void test09()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + " []");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("malformed arguments", error);
+            }
+        });
+        
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 3.3.3 - Malformed arguments case 3")
+    void test10()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + " ;!/");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("malformed arguments", error);
+            }
+        });
+        
+        customer.run();
+    }
+    
+    @Test
+    @DisplayName("Test 3.3.4 - Malformed arguments case 4")
+    void test11()
+        throws IOException
+    {
+        customer.setConnectionHandler(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + " }}}}");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertFalse(response.didSucceed());
+                
+                String error = castToString(response.getPayload());
+                
+                assertEquals("malformed arguments", error);
+            }
+        });
+        
+        customer.run();
     }
 }
