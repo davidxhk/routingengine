7cc7954b574df3e3ec65e82ba492149db7104b42
leo-dh <54761622+leo-dh@users.noreply.github.com>
2020-03-30 22:13:16 +0800

    Moved Handshake Protocol to WebSocketConnectionHandler

--- src/main/java/com/routingengine/server/ServerConnectionHandler.java
+++ src/main/java/com/routingengine/server/ServerConnectionHandler.java
@@ -4,14 +4,7 @@ import static com.routingengine.Logger.log;
 import static com.routingengine.json.JsonProtocol.JsonProtocolException;
 import java.io.Closeable;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
-import java.security.MessageDigest;
-import java.util.Base64;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import com.google.gson.JsonElement;
 import com.routingengine.MethodManager;
 import com.routingengine.RoutingEngine;
@@ -27,29 +20,6 @@ public final class ServerConnectionHandler extends WebSocketConnectionHandler
     ServerConnectionHandler(Socket socket, RoutingEngine routingEngine)
         throws IOException
     {
-        InputStream in = socket.getInputStream();
-        OutputStream out  = socket.getOutputStream();
-        Scanner s = new Scanner(in, "UTF-8");
-        try {
-            String data = s.useDelimiter("\\r\\n\\r\\n").next();
-            Matcher get = Pattern.compile("^GET").matcher(data);
-            if (get.find()) {
-                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-                match.find();
-                byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
-                    + "Connection: Upgrade\r\n"
-                    + "Upgrade: websocket\r\n"
-                    + "Sec-WebSocket-Accept: "
-                    + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
-                    + "\r\n\r\n").getBytes("UTF-8");
-                out.write(response, 0, response.length);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        // finally {
-        //     s.close();
-        // }
         connect(socket);
 
         log("Server connected to " + socket.toString());
--- src/main/java/com/routingengine/server/WebSocketConnectionHandler.java
+++ src/main/java/com/routingengine/server/WebSocketConnectionHandler.java
@@ -1,18 +1,45 @@
 package com.routingengine.server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import com.routingengine.json.JsonConnectionHandler;
-import com.routingengine.json.JsonReader;
-import com.routingengine.json.JsonWriter;
 
 public abstract class WebSocketConnectionHandler extends JsonConnectionHandler {
 
 
     @Override
+    @SuppressWarnings({ "resource" })
     public void connect(Socket socket)
         throws IOException
     {
+        // Handshake Protocol
+        InputStream in = socket.getInputStream();
+        OutputStream out  = socket.getOutputStream();
+        Scanner s = new Scanner(in, "UTF-8");
+        try {
+            String data = s.useDelimiter("\\r\\n\\r\\n").next();
+            Matcher get = Pattern.compile("^GET").matcher(data);
+            if (get.find()) {
+                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+                match.find();
+                byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
+                    + "Connection: Upgrade\r\n"
+                    + "Upgrade: websocket\r\n"
+                    + "Sec-WebSocket-Accept: "
+                    + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
+                    + "\r\n\r\n").getBytes("UTF-8");
+                out.write(response, 0, response.length);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         this.socket = socket;
         jsonReader = new JsonReaderDecoded(socket.getInputStream());
         jsonWriter = new JsonWriterEncoded(socket.getOutputStream());
