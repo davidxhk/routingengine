54eb520ad84139c5f168f507393d5c5028c1deed
han-keong <hk997@live.com>
2020-04-20 10:01:30 +0800

    finished CheckAgentMethodTest

--- src/test/java/com/routingengine/methods/CheckAgentMethodTest.java
+++ src/test/java/com/routingengine/methods/CheckAgentMethodTest.java
@@ -24,24 +24,30 @@ public class CheckAgentMethodTest extends AbstractMethodTest
     void test01()
         throws IOException, InterruptedException, ExecutionException
     {
-        final String agentUUIDString = generateNewAgent(Map.of(1, true));
+        final String rainbowId = "rainbow_agent";
+        
+        final Map<Integer, Boolean> skills = Map.of(1, true);
+        
+        final String agentUUIDString = generateNewAgent(rainbowId, skills);
         
         execute(new ClientConnectionHandler() {
             @Override
             public void runMainLoop()
                 throws IOException, InterruptedException
             {
-                checkAgent(agentUUIDString);
+                checkAgentWithUUID(agentUUIDString);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidSucceed(response);
-                
                 Agent agent = assertResponseHasAgentPayload(response);
                 
+                assertEquals(rainbowId, agent.getRainbowId());
+                
                 assertTrue(agent.ableToService(Type.of(1)));
+                
+                assertEquals(agentUUIDString, agent.getUUID().toString());
             }
         });
         
@@ -49,8 +55,43 @@ public class CheckAgentMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 2.1 - Missing uuid")
+    @DisplayName("Test 1.2 - Valid rainbow id")
     void test02()
+        throws IOException, InterruptedException, ExecutionException
+    {
+        final String rainbowId = "rainbow_agent";
+        
+        final Map<Integer, Boolean> skills = Map.of(1, true);
+        
+        final String agentUUIDString = generateNewAgent(rainbowId, skills);
+        
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                checkAgentWithRainbowId(rainbowId);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                Agent agent = assertResponseHasAgentPayload(response);
+                
+                assertEquals(rainbowId, agent.getRainbowId());
+                
+                assertTrue(agent.ableToService(Type.of(1)));
+                
+                assertEquals(agentUUIDString, agent.getUUID().toString());
+            }
+        });
+        
+        removeAgent(agentUUIDString);
+    }
+    
+    @Test
+    @DisplayName("Test 2.1 - Missing uuid or rainbow id")
+    void test03()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -66,16 +107,14 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
-                assertResponseHasErrorPayload(response, "uuid missing");
+                assertResponseHasErrorPayload(response, "uuid or rainbow id missing");
             }
         });
     }
     
     @Test
-    @DisplayName("Test 2.2.1 - Invalid uuid case 1")
-    void test03()
+    @DisplayName("Test 2.2.1 - Invalid uuid: json array")
+    void test04()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -92,16 +131,14 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
                 assertResponseHasErrorPayload(response, "uuid invalid");
             }
         });
     }
     
     @Test
-    @DisplayName("Test 2.2.2 - Invalid uuid case 2")
-    void test04()
+    @DisplayName("Test 2.2.2 - Invalid uuid: json object")
+    void test05()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -118,16 +155,14 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
                 assertResponseHasErrorPayload(response, "uuid invalid");
             }
         });
     }
     
     @Test
-    @DisplayName("Test 2.2.3 - Invalid uuid case 3")
-    void test05()
+    @DisplayName("Test 2.2.3 - Invalid uuid: non-conforming string")
+    void test06()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -144,16 +179,110 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
                 assertResponseHasErrorPayload(response, "uuid invalid");
             }
         });
     }
     
     @Test
+    @DisplayName("Test 2.2.4 - Invalid uuid: unknown uuid")
+    void test07()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("uuid", "b50544fc-c7db-4c84-ae49-297c3676c796")
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "uuid not found");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 2.3.1 - Invalid rainbow id: json array")
+    void test08()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("rainbow_id", new ArrayList<>())
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "rainbow id invalid");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 2.3.2 - Invalid rainbow id: json object")
+    void test09()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("rainbow_id", new HashMap<>())
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "rainbow id invalid");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 2.3.3 - Invalid rainbow id: unknown rainbow id")
+    void test10()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("rainbow_id", "hahaha test test")
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "rainbow id not found");
+            }
+        });
+    }
+    
+    @Test
     @DisplayName("Test 3.1 - Missing input")
-    void test06()
+    void test11()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -169,19 +298,21 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
-                assertResponseHasErrorPayload(response, "uuid missing");
+                assertResponseHasErrorPayload(response, "uuid or rainbow id missing");
             }
         });
     }
     
     @Test
-    @DisplayName("Test 3.2 - Unexpected arguments")
-    void test07()
+    @DisplayName("Test 3.2.1 - Unexpected arguments (valid uuid)")
+    void test12()
         throws IOException, InterruptedException, ExecutionException
     {
-        final String agentUUIDString = generateNewAgent(Map.of(1, true));
+        final String rainbowId = "rainbow_agent";
+        
+        final Map<Integer, Boolean> skills = Map.of(1, true);
+        
+        final String agentUUIDString = generateNewAgent(rainbowId, skills);
         
         execute(new ClientConnectionHandler() {
             @Override
@@ -198,11 +329,13 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidSucceed(response);
-                
                 Agent agent = assertResponseHasAgentPayload(response);
                 
+                assertEquals(rainbowId, agent.getRainbowId());
+                
                 assertTrue(agent.ableToService(Type.of(1)));
+                
+                assertEquals(agentUUIDString, agent.getUUID().toString());
             }
         });
         
@@ -210,8 +343,47 @@ public class CheckAgentMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 3.3.1 - Malformed arguments case 1")
-    void test08()
+    @DisplayName("Test 3.2.2 - Unexpected arguments (valid rainbow id)")
+    void test13()
+        throws IOException, InterruptedException, ExecutionException
+    {
+        final String rainbowId = "rainbow_agent";
+        
+        final Map<Integer, Boolean> skills = Map.of(1, true);
+        
+        final String agentUUIDString = generateNewAgent(rainbowId, skills);
+        
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("rainbow_id", rainbowId)
+                    .setArgument("something", "something?")
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                Agent agent = assertResponseHasAgentPayload(response);
+                
+                assertEquals(rainbowId, agent.getRainbowId());
+                
+                assertTrue(agent.ableToService(Type.of(1)));
+                
+                assertEquals(agentUUIDString, agent.getUUID().toString());
+            }
+        });
+        
+        removeAgent(agentUUIDString);
+    }
+    
+    @Test
+    @DisplayName("Test 3.3.1 - Malformed arguments: string")
+    void test14()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -226,7 +398,27 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
+                assertResponseHasErrorPayload(response, "malformed arguments");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 3.3.2 - Malformed arguments: empty string")
+    void test15()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + "");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
                 
                 assertResponseHasErrorPayload(response, "malformed arguments");
             }
@@ -234,8 +426,30 @@ public class CheckAgentMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 3.3.2 - Malformed arguments case 2")
-    void test09()
+    @DisplayName("Test 4.4.3 - Malformed arguments: numbers")
+    void test16()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + " 1234");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "malformed arguments");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 4.4.4 - Malformed arguments: json array")
+    void test17()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -250,16 +464,14 @@ public class CheckAgentMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
                 assertResponseHasErrorPayload(response, "malformed arguments");
             }
         });
     }
     
     @Test
-    @DisplayName("Test 3.3.3 - Malformed arguments case 3")
-    void test10()
+    @DisplayName("Test 4.4.5 - Malformed arguments: invalid json object")
+    void test18()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -267,38 +479,12 @@ public class CheckAgentMethodTest extends AbstractMethodTest
             public void runMainLoop()
                 throws IOException, InterruptedException
             {
-                jsonWriter.writeString(method + " ;!/");
+                jsonWriter.writeString(method + " {{{{");
                 jsonWriter.flush();
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
-                
-                assertResponseHasErrorPayload(response, "malformed arguments");
-            }
-        });
-    }
-    
-    @Test
-    @DisplayName("Test 3.3.4 - Malformed arguments case 4")
-    void test11()
-        throws IOException
-    {
-        execute(new ClientConnectionHandler() {
-            @Override
-            public void runMainLoop()
-                throws IOException, InterruptedException
-            {
-                jsonWriter.writeString(method + " }}}}");
-                jsonWriter.flush();
-                
-                JsonResponse response = awaitResponse();
-                
-                assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "malformed arguments");
             }
