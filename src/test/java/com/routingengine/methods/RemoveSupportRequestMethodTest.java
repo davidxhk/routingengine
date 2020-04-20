f2bb43472c12f696302ac2b0d45427f816c6af6d
han-keong <hk997@live.com>
2020-04-20 23:19:47 +0800

    finished RemoveSupportRequestMethodTest

--- src/test/java/com/routingengine/methods/RemoveSupportRequestMethodTest.java
+++ src/test/java/com/routingengine/methods/RemoveSupportRequestMethodTest.java
@@ -1,12 +1,17 @@
 package com.routingengine.methods;
 
+import static com.routingengine.RoutingEngine.DEFAULT_ADMIN;
+import static org.junit.Assume.assumeTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import com.routingengine.Agent;
 import com.routingengine.SupportRequest;
 import com.routingengine.client.ClientConnectionHandler;
 import com.routingengine.json.JsonRequest;
@@ -24,6 +29,18 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     {
         final String supportRequestUUIDString = generateNewSupportRequest("bob", "bob@abc.com", 1);
         
+        customerWaitsForAgent(supportRequestUUIDString);
+        
+        final String agentUUIDString = generateNewAgent("rainbow_agent", Map.of(1, true));
+        
+        agentUpdatesAvailability(agentUUIDString, true);
+        
+        agentTakesSupportRequest(agentUUIDString);
+        
+        TimeUnit.MILLISECONDS.sleep(10);
+        
+        assumeTrue(agentDidTakeSupportRequest(agentUUIDString, supportRequestUUIDString));
+        
         execute(new ClientConnectionHandler() {
             @Override
             public void runMainLoop()
@@ -35,8 +52,6 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidSucceed(response);
-                
                 SupportRequest supportRequest = assertResponseHasSupportRequestPayload(response);
                 
                 assertFalse(supportRequest.isOpen());
@@ -47,11 +62,23 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 
                 response = awaitResponse();
                 
-                assertResponseDidNotSucceed(response);
+                assumeResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "uuid not found");
+                
+                checkAgentWithUUID(agentUUIDString);
+                
+                response = awaitResponse();
+                
+                Agent agent = assumeResponseHasAgentPayload(response);
+                
+                assertTrue(agent.isAvailable());
+                
+                assertFalse(agent.hasAssignedSupportRequest());
             }
         });
+        
+        agentGetsRemoved(agentUUIDString);
     }
     
     @Test
@@ -66,13 +93,12 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
             {
                 new JsonRequest()
                     .setMethod(method)
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
                     .writeTo(jsonWriter);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "uuid missing");
             }
@@ -80,7 +106,7 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 2.2.1 - Invalid uuid case 1")
+    @DisplayName("Test 2.2.1 - Invalid uuid: json array")
     void test03()
         throws IOException
     {
@@ -92,13 +118,12 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 new JsonRequest()
                     .setMethod(method)
                     .setArgument("uuid", new ArrayList<>())
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
                     .writeTo(jsonWriter);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "uuid invalid");
             }
@@ -106,7 +131,7 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 2.2.2 - Invalid uuid case 2")
+    @DisplayName("Test 2.2.2 - Invalid uuid: json object")
     void test04()
         throws IOException
     {
@@ -118,13 +143,12 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 new JsonRequest()
                     .setMethod(method)
                     .setArgument("uuid", new HashMap<>())
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
                     .writeTo(jsonWriter);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "uuid invalid");
             }
@@ -132,7 +156,7 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 2.2.3 - Invalid uuid case 3")
+    @DisplayName("Test 2.2.3 - Invalid uuid: non-conforming string")
     void test05()
         throws IOException
     {
@@ -144,13 +168,12 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 new JsonRequest()
                     .setMethod(method)
                     .setArgument("uuid", "hahaha test test")
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
                     .writeTo(jsonWriter);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "uuid invalid");
             }
@@ -158,7 +181,7 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 3.1 - Missing input")
+    @DisplayName("Test 2.2.4 - Invalid uuid: unknown uuid")
     void test06()
         throws IOException
     {
@@ -169,13 +192,37 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
             {
                 new JsonRequest()
                     .setMethod(method)
+                    .setArgument("uuid", "b50544fc-c7db-4c84-ae49-297c3676c796")
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
                     .writeTo(jsonWriter);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
+                assertResponseHasErrorPayload(response, "uuid not found");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 3.1 - Missing input")
+    void test07()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
                 
                 assertResponseHasErrorPayload(response, "uuid missing");
             }
@@ -183,11 +230,65 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 3.2 - Unexpected arguments")
-    void test07()
+    @DisplayName("Test 3.2 - Missing admin uuid")
+    void test08()
         throws IOException, InterruptedException, ExecutionException
     {
         final String supportRequestUUIDString = generateNewSupportRequest("bob", "bob@abc.com", 1);
+        
+        customerWaitsForAgent(supportRequestUUIDString);
+        
+        final String agentUUIDString = generateNewAgent("rainbow_agent", Map.of(1, true));
+        
+        agentUpdatesAvailability(agentUUIDString, true);
+        
+        agentTakesSupportRequest(agentUUIDString);
+        
+        TimeUnit.MILLISECONDS.sleep(10);
+        
+        assumeTrue(agentDidTakeSupportRequest(agentUUIDString, supportRequestUUIDString));
+        
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                new JsonRequest()
+                    .setMethod(method)
+                    .setArgument("uuid", supportRequestUUIDString)
+                    .writeTo(jsonWriter);
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "admin uuid missing");
+            }
+        });
+        
+        supportRequestGetsRemoved(supportRequestUUIDString);
+        
+        agentGetsRemoved(agentUUIDString);
+    }
+    
+    @Test
+    @DisplayName("Test 3.3 - Unexpected arguments")
+    void test09()
+        throws IOException, InterruptedException, ExecutionException
+    {
+        final String supportRequestUUIDString = generateNewSupportRequest("bob", "bob@abc.com", 1);
+        
+        customerWaitsForAgent(supportRequestUUIDString);
+        
+        final String agentUUIDString = generateNewAgent("rainbow_agent", Map.of(1, true));
+        
+        agentUpdatesAvailability(agentUUIDString, true);
+        
+        agentTakesSupportRequest(agentUUIDString);
+        
+        TimeUnit.MILLISECONDS.sleep(10);
+        
+        assumeTrue(agentDidTakeSupportRequest(agentUUIDString, supportRequestUUIDString));
         
         execute(new ClientConnectionHandler() {
             @Override
@@ -198,13 +299,12 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                     .setMethod(method)
                     .setArgument("uuid", supportRequestUUIDString)
                     .setArgument("something", "something?")
+                    .setArgument("admin_uuid", DEFAULT_ADMIN)
                     .writeTo(jsonWriter);
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidSucceed(response);
                 
                 SupportRequest supportRequest = assertResponseHasSupportRequestPayload(response);
                 
@@ -216,16 +316,28 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 
                 response = awaitResponse();
                 
-                assertResponseDidNotSucceed(response);
+                assumeResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "uuid not found");
+                
+                checkAgentWithUUID(agentUUIDString);
+                
+                response = awaitResponse();
+                
+                Agent agent = assumeResponseHasAgentPayload(response);
+                
+                assertTrue(agent.isAvailable());
+                
+                assertFalse(agent.hasAssignedSupportRequest());
             }
         });
+        
+        agentGetsRemoved(agentUUIDString);
     }
     
     @Test
-    @DisplayName("Test 3.3.1 - Malformed arguments case 1")
-    void test08()
+    @DisplayName("Test 3.4.1 - Malformed arguments: string")
+    void test10()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -240,7 +352,27 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
+                assertResponseHasErrorPayload(response, "malformed arguments");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 3.4.2 - Malformed arguments: empty string")
+    void test11()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + "");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
                 
                 assertResponseHasErrorPayload(response, "malformed arguments");
             }
@@ -248,8 +380,30 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
     }
     
     @Test
-    @DisplayName("Test 3.3.2 - Malformed arguments case 2")
-    void test09()
+    @DisplayName("Test 3.4.3 - Malformed arguments: numbers")
+    void test12()
+        throws IOException
+    {
+        execute(new ClientConnectionHandler() {
+            @Override
+            public void runMainLoop()
+                throws IOException, InterruptedException
+            {
+                jsonWriter.writeString(method + " 1234");
+                jsonWriter.flush();
+                
+                JsonResponse response = awaitResponse();
+                
+                assertEquals(method, response.getMethod());
+                
+                assertResponseHasErrorPayload(response, "malformed arguments");
+            }
+        });
+    }
+    
+    @Test
+    @DisplayName("Test 3.4.4 - Malformed arguments: json array")
+    void test13()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -264,16 +418,14 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
                 
                 assertEquals(method, response.getMethod());
                 
-                assertResponseDidNotSucceed(response);
-                
                 assertResponseHasErrorPayload(response, "malformed arguments");
             }
         });
     }
     
     @Test
-    @DisplayName("Test 3.3.3 - Malformed arguments case 3")
-    void test10()
+    @DisplayName("Test 3.4.5 - Malformed arguments: invalid json object")
+    void test14()
         throws IOException
     {
         execute(new ClientConnectionHandler() {
@@ -281,38 +433,12 @@ public class RemoveSupportRequestMethodTest extends AbstractMethodTest
             public void runMainLoop()
                 throws IOException, InterruptedException
             {
-                jsonWriter.writeString(method + " ;!/");
+                jsonWriter.writeString(method + " {{{{");
                 jsonWriter.flush();
                 
                 JsonResponse response = awaitResponse();
                 
                 assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
-                
-                assertResponseHasErrorPayload(response, "malformed arguments");
-            }
-        });
-    }
-    
-    @Test
-    @DisplayName("Test 3.3.4 - Malformed arguments case 4")
-    void test11()
-        throws IOException
-    {
-        execute(new ClientConnectionHandler() {
-            @Override
-            public void runMainLoop()
-                throws IOException, InterruptedException
-            {
-                jsonWriter.writeString(method + " }}}}");
-                jsonWriter.flush();
-                
-                JsonResponse response = awaitResponse();
-                
-                assertEquals(method, response.getMethod());
-                
-                assertResponseDidNotSucceed(response);
                 
                 assertResponseHasErrorPayload(response, "malformed arguments");
             }
