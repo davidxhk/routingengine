315c38abc33a1ccd08a7ee04bd149b04dbeaf038
han-keong <hk997@live.com>
2020-04-21 14:22:52 +0800

    updated module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,18 @@
 module com.routingengine
 {
-    // Features
-    exports com.routingengine;          // Core logic of the routing engine
-    exports com.routingengine.methods;  // Methods that are used by the routing engine
-    exports com.routingengine.server;   // Server program for the routing engine
-    exports com.routingengine.client;   // Client interface to communicate with the routing engine server
-    exports com.routingengine.json;     // JSON tools and protocol that will be useful for development
+    exports com.routingengine.websocket;
+    exports com.routingengine;
+    exports com.routingengine.server;
+    exports com.routingengine.examples;
+    exports com.routingengine.json;
+    exports com.routingengine.client;
+    exports com.routingengine.methods;
     
-    // Dependencies
     requires transitive com.google.gson;
-    
-    // For testing purposes
+    requires java.logging;
+    requires java.sql;
+    requires junit;
     requires org.junit.jupiter.api;
-    requires org.junit.platform.suite.api;
     requires org.junit.platform.runner;
-    opens com.routingengine;
-    opens com.routingengine.methods;
+    requires org.junit.platform.suite.api;
 }
